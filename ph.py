8636df36abc269d7d2ce94dcf4142ab52c693065
IvanTurgenev <kenneth19949@gmail.com>
2016-03-05 18:09:57 -0500

    Think fixed hell

--- ph.py
+++ ph.py
@@ -20,6 +20,25 @@ def push(x):
 
 #sys.exit()
 
+def addb(dic,key,val):
+	dic.setdefault(key, [])
+	dic[key].append(val)
+
+#def remb(dic,key):
+#	del (dic[key])[0]
+
+def ctb(ids,vb) :
+	mac = nmac()
+	matchv = list(set(kid).intersection(mac))
+	matchf = list(set(kid).difference(matchv))
+
+	for key in matchv:
+		addb(vb,key,True)
+	for key in matchf:
+		addb(vb,key,False)
+	
+	return (vb)
+
 #ids csv file of MACADDRESS,DEVICENAME
 ids = {}
 for key, val in csv.reader(open("ids.csv")):
@@ -27,46 +46,79 @@ for key, val in csv.reader(open("ids.csv")):
 
 kid = list(ids.keys())
 
-mac = nmac()
-
-matchv = list(set(kid).intersection(mac))
-
-matchf = list(set(kid).difference(matchv))
-
 vb = {}
-for key in matchv:
-	vb[key] = True
-for key in matchf:
-	vb[key] = False
+vb1 = {}
 
+count = 1
+while True :
+	
+	if not vb1:
+		vb = ctb(kid,vb)	
+	else:
+		vb = vb1
+		vb1 = {}
+
+	if count == 10 :
+		vbr = {}
+		for key in vb:
+			if True in vb[key]:
+				vbr[key] = True
+			else:
+				vbr[key] = False
+				
+		while count != 1:
+			vb1 = ctb(kid,vb1)
+			count -= 1
+			print("vb1")
+			print(vb1)
+			time.sleep(60)
+			if count == 1:
+				vbr1 = {}
+				for key in vb1:
+					if True in vb1[key]:
+						vbr1[key] = True
+					else:
+						vbr1[key] = False
+				chan = [k for k in vbr if vbr[k] != vbr1[k]]
+				print("chan")
+				print(chan)
+				for key in chan:
+					if vb1[key] == True:
+						push(ids[key] + HASEN)
+					else:
+						push(ids[key] + HASLE)
+	count += 1
+	print("vb")
+	print(vb)
+	time.sleep(60)
 
 #----------------
 #bolean dictionaries
-def loop(bc):
-	time.sleep(60)
+#def loop(bc):
+#	time.sleep(60)
 
-	mac1 = nmac()
+#	mac1 = nmac()
 
-	matchv1 = list(set(kid).intersection(mac1))
+#	matchv1 = list(set(kid).intersection(mac1))
 
-	matchf1 = list(set(kid).difference(matchv1))
+#	matchf1 = list(set(kid).difference(matchv1))
 
-	vb1 = {}
-	for key in matchv1:
-        	vb1[key] = True
-	for key in matchf1:
-        	vb1[key] = False
+#	vb1 = {}
+#	for key in matchv1:
+#        	vb1[key] = True
+#	for key in matchf1:
+#       	vb1[key] = False
 
 	#vb1 = dict(vb)
 	#False = vb1['MACADDDRES']
-	chan = [k for k in bc if bc[k] != vb1[k]]
+#	chan = [k for k in bc if bc[k] != vb1[k]]
 
-	print (chan)
-	for key in chan:
-		if vb1[key] == True:
-			push(ids[key] + HASEN) 
-		else:
-			push(ids[key] + HASLE)
-	loop(vb1)
+#	print (chan)
+#	for key in chan:
+#		if vb1[key] == True:
+#			push(ids[key] + HASEN) 
+#		else:
+#			push(ids[key] + HASLE)
+#	loop(vb1)
 
-loop(vb)
+#loop(vb)
