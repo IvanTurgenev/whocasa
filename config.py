aa8d4a2d394217684d5f55bc31678fbeed692778
IvanTurgenev <kenneth19949@gmail.com>
2016-08-01 17:20:35 -0500

    fixed untracked files

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py:                                        
-ipr = "192.169.1.0"                                 
-
-API_KEY = "YOUR KEY"
-CHANNEL_NAME = "YOUR CHANNEL NAME"                            
-# string for has leaved and has entered             
-HASEN = " ha entrado"                               
-HASLE = " se ha ido"                                
