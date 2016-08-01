eb9dffd501ae4b7e3979bbd8dc94bf699d24e871
IvanTurgenev <kenneth19949@gmail.com>
2016-08-01 16:48:41 -0500

    Added log capabilities, added customized ip range

--- ph.py
+++ ph.py
@@ -1,3 +1,5 @@
+
+import os
 import subprocess
 import csv
 import sys
@@ -5,7 +7,9 @@ import time
 import config
 from pushetta import Pushetta
 
-nmap = "nmap -sn 192.168.1.0/24 | grep --color=never -Po 'Address: \K.[^ ]*'"
+
+ipr = config.ipr
+nmap = "nmap -sn " + ipr + " | grep --color=never -Po 'Address: \K.[^ ]*'"
 
 
 def nmac():
@@ -16,8 +20,9 @@ def nmac():
     # print("NMAC X POP")
     # print(x.pop())
     except subprocess.CalledProcessError as e:
-        print(e.output)
-        print("retrying in 60 seconds")
+        cfl(fln, [e.output, "retrying in 60 seconds"])
+        # print(e.output)
+        # print("retrying in 60 seconds")
         time.sleep(60)
         x = nmac()
     x.pop()
@@ -65,11 +70,6 @@ def opcsv(nmbr):
     return(list(ids.keys()), ids)
 
 
-def prk(vbx):
-    for key in vbx:
-        print(vbx[key])
-
-
 def crvbr(vbx, vbrx):
     for key in vbx:
         if True in vbx[key]:
@@ -84,6 +84,7 @@ def earlnot(vbrx, vbx, arlnotx):
         if ((vbrx[key] is False) and
                 (True in vbx[key]) and (key not in arlnotx)):
             push(ids[key] + config.HASEN + " " + tme)
+            cfl(fln, [ids[key] + config.HASEN + " " + tme])
             arlnotx.append(key)
     return(arlnotx)
 
@@ -92,8 +93,36 @@ def tmee():
     return(time.strftime("%H:%M:%S"))
 
 
+def nlog():
+    fln = time.strftime("%d %m %Y")
+    if os.path.isfile(fln):
+        count = 1
+        while True:
+            if os.path.isfile(fln + " #" + str(count)) is False:
+                return(fln + " #" + str(count))
+            else:
+                count += 1
+    else:
+        return(fln)
+
+
+def cfl(fln, ln):
+    with open(fln, 'a') as f:
+        f.write("\n".join(ln))
+        print("\n".join(ln))
+
+
+def lstvb(idsx, vbx):
+    lst = []
+    for key in vbx:
+        lst.append(str(idsx[key]))
+        lst.append(str(vbx[key]))
+    return(lst)
+
+
 if __name__ == '__main__':
 
+    fln = nlog()
     (kid, ids) = opcsv("ids.csv")
     # print("KID")
     # print(kid)
@@ -101,10 +130,10 @@ if __name__ == '__main__':
     vb1 = {}
     count = 1
     while True:
-        print(count)
+        # print(count)
         if not vb1:
             vb = ctb(kid, vb)
-            print("TABLA DE VERDAD")
+            # print("VB")
             # print(vb)
         else:
             vb = dict(vb1)
@@ -118,15 +147,17 @@ if __name__ == '__main__':
             arlnot = []
             while count != 1:
                 vb1 = ctb(kid, vb1)
-                print("TABLA DE VERDAD 1")
-                count -= 1
+                # print("TABLA DE VERDAD 1")
                 arlnot = earlnot(vbr, vb1, arlnot)
-                print("vb1")
+                # print("vb1")
                 # print(vb1)
-                print(time.strftime("%H:%M:%S"))
-                for key in vb1:
-                    print(ids[key])
-                    print(vb1[key])
+                # print(time.strftime("%H:%M:%S"))
+                # for key in vb1:
+                # print(ids[key])
+                # print(vb1[key])
+                vbl1 = lstvb(ids, vb1)
+                cfl(fln, ["vb1", tmee(), "#cicle " + str(count)] + vbl1)
+                count -= 1
                 time.sleep(60)
                 if count == 1:
                     vbr1 = {}
@@ -141,16 +172,16 @@ if __name__ == '__main__':
                     tme = tmee()
                     for key in chan:
                         # print(ids[key])
-                        if vbr1[key]:
-                            print(ids[key] + config.HASEN + " NOT NOF " + tme)
-                            # push(ids[key] + config.HASEN + " " + tme)
-                        else:
+                        if not vbr1[key]:
                             push(ids[key] + config.HASLE + " " + tme)
+                        else:
+                            ntf = [ids[key] + config.HASEN + " NOT NOF " + tme]
+                            cfl(fln, ntf)
+                        # print(ids[key] + config.HASEN + " NOT NOF"+tme)
+                        # push(ids[key] + config.HASEN + " " + tme)
+        vbl = lstvb(ids, vb)
+        cfl(fln, ["vb", tmee(), "#cicle " + str(count)] + vbl)
         count += 1
-        print("vb")
-        print(time.strftime("%H:%M:%S"))
-        for key in vb:
-            print(ids[key])
-            print(vb[key])
-        # print(vb)
+        # print("vb")
+        # print(time.strftime("%H:%M:%S"))
         time.sleep(60)
