ffecd0c8752a7d7e1a5e54bec310ec6ed770553c
IvanTurgenev <kenneth19949@gmail.com>
2016-08-01 21:14:29 -0500

    updated confgi

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 # config.py:
+ipr = "192.168.1.0"
 
-API_KEY="YOUR KEY"
-CHANNEL_NAME="YOUR CHANNEL NAME"
-#string for has leaved and has entered
-HASEN="has entered"
-HASLE="has leaved"
+API_KEY = "YOUR KEY"
+CHANNEL_NAME = "YOUR CHANNEL NAME"
+
+# string for has leaved and has entered
+
+HASEN = "has entered"
+HASLE = "has leaved"
